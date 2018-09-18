feeb9a0bc124485f8a35f56a5c20d43d4995d11e
Julius Btesh <julius.btesh@mangoconcept.com>
2018-09-18 10:06:58 -0400

    Initial Commit

---
+++ weather/FirstViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FirstViewController.swift
+//  weather
+//
+//  Created by Julius Btesh on 9/18/18.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ weather/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  weather
+//
+//  Created by Julius Btesh on 9/18/18.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
